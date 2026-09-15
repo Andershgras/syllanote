fdb20be7f24ee30fa200e99b2ff6a1df71455b6a
Andershgras <andershgras@gmail.com>
2026-09-15 14:35:28 +0200

    Configure application infrastructure

---
+++ Syllanote/src/Syllanote.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Syllanote.Infrastructure.Persistence;
+
+namespace Syllanote.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        string connectionString)
+    {
+        services.AddDbContext<SyllanoteDbContext>(options =>
+            options.UseSqlite(connectionString));
+
+        return services;
+    }
+}
---
+++ Syllanote/src/Syllanote.Infrastructure/Persistence/SyllanoteDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Syllanote.Infrastructure.Persistence;
+
+public class SyllanoteDbContext : DbContext
+{
+    public SyllanoteDbContext(DbContextOptions<SyllanoteDbContext> options)
+        : base(options)
+    {
+    }
+}
